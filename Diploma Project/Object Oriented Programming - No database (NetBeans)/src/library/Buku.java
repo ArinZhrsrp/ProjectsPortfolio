51c85f23ff1a43d49d4fc9fed948172d84d56b35
ArinZhrsrp <107144285+ArinZhrsrp@users.noreply.github.com>
2023-01-16 10:08:59 +0800

    Add files via upload

---
+++ Diploma Project/Object Oriented Programming - No database (NetBeans)/src/library/Buku.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Shiwwin Zahra
+ */
+public interface Buku {
+    
+       public void deletefromfile(String word );
+       public void writetofile(ArrayList<String> list);
+
+}
